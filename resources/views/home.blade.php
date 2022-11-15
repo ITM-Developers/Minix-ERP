230bf2962bab44187315452e18799657a9a15dff
BayronVazquez <BayronVazquez@gmail.com>
2022-11-15 08:53:53 -0600

    Add Theme of NiceAdmin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.themes.NiceAdmin.app')
+
+@section('page-title','Home')
+@section('content')
+<h1>Some content</h1>
+{{-- <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+
+                    <script>
+                        window.addEventListener('load', (event) => {
+                            Swal.fire(
+                                'Good job!',
+                                'You clicked the button!',
+                                'success'
+                            )
+                        });
+                    </script>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+@endsection
